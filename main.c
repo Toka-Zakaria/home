446a8d24df3f73d018de9db20d77a4cc8c251634
Toka-Zakaria <75904835+Toka-Zakaria@users.noreply.github.com>
2022-08-07 22:50:27 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,31 @@
+#include"SWUART.h"
+#include"Timer0.h"
+#include"platform.h"
+#define DATA        10
+
+
+#define  MC_READY   0x11 /*to mak sure it will send & receive correctly*/
+
+int main(void)
+{
+uint8_t check = 0;
+  
+  /*Initialization*/
+  SWUART_init(9600);
+  
+  
+  /*Application*/
+  /*Cyclic Executive*/
+  while(1)
+  {
+    
+    while ( check != MC_READY ) 
+    {
+      SWUART_receive(&check) ;
+    };
+    SWUART_send(DATA);
+    
+  }
+    
+  return 0;
+}
